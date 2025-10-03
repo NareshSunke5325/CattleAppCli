b02f13e823d7abd63141aeca0dc9e86e6b85fec2
NareshSunke5325 <165786055+NareshSunke5325@users.noreply.github.com>
2025-10-03 12:47:51 +0530

    initial commit

--- src/theme/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Color } from './Color';
-export { default as Fonts } from './Fonts';
-export { default as Metrics } from './Metrics';
-export { default as Styles } from './Styles';
